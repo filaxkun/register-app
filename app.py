5fb1e65460e1c7d03d0483e0ffe140ef7803e37f
Filax <fls.francesco@gmail.com>
2020-05-29 09:56:58 +0200

    Turning greetings into a register app
    - Added register page
    - Added simple control for inputs and landing pages (success/failure)

--- app.py
+++ app.py
@@ -2,8 +2,21 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+people = []
+
 @app.route("/")
 def index():
-    name     = request.args.get("name","PD")
-    greeting = request.args.get("greeting","hello")
-    return render_template("index.html", name=name, greeting=greeting)
+    return render_template("index.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+    name     = request.form.get("name")
+    activity = request.form.get("activity")
+    if not name or not activity:
+        return render_template("failure.html")
+    #people.append(f"{name} subbed to {activity}")
+    return render_template("success.html",name=name,activity=activity)
+
+@app.route("/registrants")
+def show_registrants():
+    return render_teamplate("registrants.html",people=people)
